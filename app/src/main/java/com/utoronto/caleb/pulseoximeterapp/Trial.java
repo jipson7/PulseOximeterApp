5c535149fdbf24cb4345e36cb2b8b2d569abe929
Caleb Phillips <calebrphillips@gmail.com>
2018-03-14 22:42:50 -0400

    Add trial class for firebase

---
+++ app/src/main/java/com/utoronto/caleb/pulseoximeterapp/Trial.java
@@ -0,0 +1,29 @@
+package com.utoronto.caleb.pulseoximeterapp;
+
+import com.google.firebase.database.Exclude;
+import com.google.firebase.database.IgnoreExtraProperties;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@IgnoreExtraProperties
+public class Trial {
+    public String name;
+    public String description;
+    public long start;
+    @Exclude
+    public long end;
+
+    public Trial(){}
+
+    public Trial(String name, String description) {
+        this.name = name;
+        this.description = description;
+        this.start = System.currentTimeMillis();
+    }
+
+    @Exclude
+    public void end() {
+        this.end = System.currentTimeMillis();
+    }
+}
