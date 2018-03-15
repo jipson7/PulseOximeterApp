bf7454599e0d2b86290dca9a42a45d95cbd893d8
Caleb Phillips <calebrphillips@gmail.com>
2018-03-14 22:43:55 -0400

    Add test save and listener

--- app/src/main/java/com/utoronto/caleb/pulseoximeterapp/DBHelper.java
+++ app/src/main/java/com/utoronto/caleb/pulseoximeterapp/DBHelper.java
@@ -1,13 +1,32 @@
 package com.utoronto.caleb.pulseoximeterapp;
 
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class DBHelper {
 
-    final FirebaseDatabase database = FirebaseDatabase.getInstance();
+    static final String TAG = "DBHELPER";
+
+    Trial mTrial;
+
+    String collectionName = "trials";
+
+    DatabaseReference mTrialsRef;
 
     public DBHelper() {
-        //TODO Setup trial (use class) here.
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        mTrialsRef = database.getReference(collectionName);
+        //TODO replace defaults
+        String name = "defaultTrial";
+        String description = "defaultDesc";
+        mTrial = new Trial(name, description);
     }
 
     public void saveData(int hr, int spo2, int bp, long timestamp, Device device) {
@@ -15,6 +34,20 @@ public class DBHelper {
     }
 
     public void endSession() {
-        //TODO cleanup
+        mTrial.end();
+    }
+
+    public void testSave() {
+        Log.d(TAG, "ATTEMPTING TO SAVE DATA.");
+        mTrialsRef.push().setValue(mTrial, new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError != null) {
+                    Log.d(TAG,"Data could not be saved " + databaseError.getMessage());
+                } else {
+                    Log.d(TAG,"Data saved successfully.");
+                }
+            }
+        });
     }
 }
