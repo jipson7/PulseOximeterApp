ee6a282bf33bc133eb7a8ce57cde97c0f16021cc
Caleb Phillips <calebrphillips@gmail.com>
2018-03-16 21:20:47 -0400

    Remove unused data keys

--- app/src/main/java/com/utoronto/caleb/pulseoximeterapp/storage/DataKeys.java
+++ app/src/main/java/com/utoronto/caleb/pulseoximeterapp/storage/DataKeys.java
@@ -4,9 +4,6 @@ public class DataKeys {
     final public static String RED = "red_led";
     final public static String IR = "ir_led";
     final public static String HR = "hr";
-    final public static String HR_VALID = "hr_valid";
     final public static String OXYGEN = "oxygen";
-    final public static String OXYGEN_VALID = "oxygen_valid";
     final public static String BP = "bp";
-    final public static String BP_VALID = "bp_valid";
 }
