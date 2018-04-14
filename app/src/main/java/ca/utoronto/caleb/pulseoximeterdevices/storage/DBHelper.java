f842db093f35694fa71739c0c8ec8b20eab948c8
Caleb Phillips <calebrphillips@gmail.com>
2018-04-14 18:53:10 -0400

    Flatten data to access trials more efficiently

--- app/src/main/java/ca/utoronto/caleb/pulseoximeterdevices/storage/DBHelper.java
+++ app/src/main/java/ca/utoronto/caleb/pulseoximeterdevices/storage/DBHelper.java
@@ -12,17 +12,17 @@ public class DBHelper {
 
     static final String TAG = "DBHELPER";
 
-    Trial mTrial;
+    DatabaseReference db;
 
-    FirebaseDatabase db;
-
-    DatabaseReference mTrialRef;
+    DatabaseReference mTrialDataRef;
 
     Map<Device, DatabaseReference> mDataRefs;
 
+    String mTrialKey;
+
     public DBHelper() {
         mDataRefs = new HashMap<>();
-        db = FirebaseDatabase.getInstance();
+        db = FirebaseDatabase.getInstance().getReference();
     }
 
     public void saveData(Device device, Map<String, Object> data) {
@@ -32,7 +32,7 @@ public class DBHelper {
         DatabaseReference dataRef = mDataRefs.get(device);
 
         if (dataRef == null) {
-            DatabaseReference deviceRef = mTrialRef.child("devices").push();
+            DatabaseReference deviceRef = mTrialDataRef.child("devices").push();
             deviceRef.setValue(device.toMap());
             dataRef = deviceRef.child("data");
             mDataRefs.put(device, dataRef);
@@ -42,14 +42,18 @@ public class DBHelper {
     }
 
     public void endSession() {
-        Map<String, Object> timeUpdate = new HashMap<>();
-        timeUpdate.put("end", System.currentTimeMillis());
-        mTrialRef.updateChildren(timeUpdate);
+        Long end =  System.currentTimeMillis();
+        db.child("/trials/" + mTrialKey).child("end").setValue(end);
+        db.child("/trials-data/" + mTrialKey).child("end").setValue(end);
     }
 
     public void setupTrial(String trialDesc) {
-        mTrial = new Trial(trialDesc);
-        mTrialRef = db.getReference("trials").push();
-        mTrialRef.setValue(mTrial.toMap());
+        Map<String, Object> trialData = (new Trial(trialDesc)).toMap();
+        mTrialDataRef = db.child("trials-data").push();
+        mTrialKey = mTrialDataRef.getKey();
+        Map<String, Object> updates = new HashMap<>();
+        updates.put("/trials-data/" + mTrialKey, trialData);
+        updates.put("/trials/" + mTrialKey, trialData);
+        db.updateChildren(updates);
     }
 }
